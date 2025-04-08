9cc97685b1fcf231d9e2a638be51fe52e5bd08c4
viglino <jean-marc.viglino@ign.fr>
2025-04-08 12:47:02 +0200

    [UPD] 3D buildings

--- src/format/carte.js
+++ src/format/carte.js
@@ -6,8 +6,9 @@ import WMTSFormat from './source/WMTS'
 import geoportailFormat from './source/geoportail'
 import vectorFormat from './source/vector'
 
+import * as itowns from '../itowns/itowns'
+
 import statFormat from 'mcutils/format/layer/Statistic'
-import Statistic from 'mcutils/layer/Statistic'
 import VectorStyle from 'mcutils/format/layer/VectorStyle'
 
 /** Read a new carte 
@@ -41,7 +42,44 @@ import VectorStyle from 'mcutils/format/layer/VectorStyle'
         break;
       }
       case 'MVT': {
-        globe.addLayer(layerFormat(MVTFormat(l), l, { addLabelLayer: true }))
+        // BDTopo 3D buildings
+        if (l.url === "https://data.geopf.fr/annexes/ressources/vectorTiles/styles/BDTOPO/bati.json") {
+          // Building data source
+          const buildingsSource = new itowns.VectorTilesSource({
+            //style: "https://data.geopf.fr/annexes/ressources/vectorTiles/styles/PLAN.IGN/standard.json",
+            style: l.url,
+            // We only want to display buildings related data.
+            filter: (layer) => {
+              return layer['source-layer'].includes('batiment')
+                  && layer.paint["fill-color"];
+            },
+          });
+          // FeatureGeometryLayer to support building data.
+          const buildingsLayer = new itowns.FeatureGeometryLayer('VTBuilding',{
+            source: buildingsSource,
+            zoom: { min: 15 },
+            accurate: false,
+            style: {
+              fill: {
+                //base_altitude: (p) => p.alti_sol || 0,
+                base_altitude: (p) => (p.altitude_minimale_sol || 0) * globe.getElevationScale(),
+                extrusion_height: (p) => (p.hauteur || 0) * globe.getElevationScale(),
+              }
+            },
+          });
+          // info
+          buildingsLayer.info.title = l.title
+          buildingsLayer.info.description = l.description
+          // options
+          buildingsLayer.visible = l.visiblility !== false;
+          buildingsLayer.opacity = l.opacity
+          
+          // console.log(buildingsLayer)
+          // Add the FeatureGeometryLayer to the scene
+          globe.addLayer(buildingsLayer)
+        } else {
+          globe.addLayer(layerFormat(MVTFormat(l), l, { addLabelLayer: true }))
+        }
         break;
       }
       case 'WMS': {
